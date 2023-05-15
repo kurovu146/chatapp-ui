1661126068d741bdd5ed6ba4f91c859edd56716d
vutuanyt146 <kurobakaito146@gmail.com>
2023-05-15 14:36:59 +0700

    add form login & register

--- src/layouts/content/Content.tsx
+++ src/layouts/content/Content.tsx
@@ -1,19 +1,39 @@
 import Button from "../../components/button/Button";
-import '../../styles/layouts/Content.css'
+import '../../styles/layouts/Content.css';
+
+const contents = [
+  {
+    id: 1,
+    message: 'Instagram post templates. Pick out ready-to-publish Instagram posts or create your own with our collection of customizable templates, icons and images.'
+  },
+  {
+    id: 2,
+    message: 'Instagram post templates. Pick out ready-to-publish Instagram posts or create your own with our collection of customizable templates, icons and images.'
+  },
+  {
+    id: 1,
+    message: 'Instagram post templates. Pick out ready-to-publish Instagram posts or create your own with our collection of customizable templates, icons and images.'
+  },
+  {
+    id: 2,
+    message: 'Instagram post templates. Pick out ready-to-publish Instagram posts or create your own with our collection of customizable templates, icons and images.'
+  }
+]
 
 function Content() {
 
-  function handleClick() {
-
-  }
   return (
     <div className="w-full flex-col justify-between">
-      <div className="content h-5/6 ">
-        Content
+      <div className="content h-5/6 flex flex-col">
+        {contents.map((content, index) => {
+          return <div key={index} className={'flex grid ' + ((content.id & 1) ? 'justify-items-start' : 'justify-items-end')}>
+            <div className="max-w-2xl border-solid border-2 rounded-xl p-2 m-2">{content.message}</div>
+          </div>
+        })}
       </div>
       <div className="message flex h-1/6 p-8">
-        <input type="text" placeholder="Text something ..." className="w-11/12 m-2 rounded"/>
-        <Button content="Send" onClick={handleClick} className="m-2 w-16 rounded bg-zinc-600 text-white border-solid hover:bg-zinc-600" />
+        <input type="text" placeholder="Text your message ..." className="w-11/12 m-2 rounded"/>
+        <Button content="Send" onClick={() => {}} className="m-2 w-16 rounded bg-zinc-600 text-white border-solid hover:bg-zinc-600" />
       </div>
     </div>
   );
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,35 @@
 import Header from "../layouts/header/Header";
+import '../styles/pages/Login.css';
 
 function Login() {
   return (
-    <div>
+    <div className="h-full">
       <Header />
-      Login
+      <div className="flex-row p-48">
+        <form className="shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="mb-4">
+            <label className="block text-white-700 text-sm font-bold mb-2" htmlFor="username" />
+              Username
+            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+          </div>
+          <div className="mb-6">
+            <label className="block text-white-700 text-sm font-bold mb-2" htmlFor="password" />
+              Password
+            <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************" />
+          </div>
+          <div className="flex items-center justify-between">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+              Sign In
+            </button>
+            <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="root">
+              Forgot Password?
+            </a>
+          </div>
+        </form>
+        <p className="text-center text-white-500 text-xs">
+          &copy;2023 Kuro. All rights reserved.
+        </p>
+      </div>
     </div>
   );
 }
